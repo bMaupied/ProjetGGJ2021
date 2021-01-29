393210bc18cf9ef339c063955a41448db7855598
Maupied <b.maupied@student.isartdigital.com>
2021-01-29 22:05:08 +0100

    Delete C++ oriented project

--- Source/ProjetGGJ2021/ProjetGGJ2021.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ProjetGGJ2021.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjetGGJ2021, "ProjetGGJ2021" );
-
-DEFINE_LOG_CATEGORY(LogProjetGGJ2021)
- 
